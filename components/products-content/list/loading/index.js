7b023c824179da1b762c7c0b9d38035a097f557b
Vitalii Tsarov <vitalii@fligmax.pl>
2022-06-13 21:36:19 +0200

    0.0.9 - Changing the appearance of the skeleton

--- components/products-content/list/loading/index.js
+++ components/products-content/list/loading/index.js
@@ -1,15 +1,21 @@
-import {Box, Skeleton} from "@mui/material";
+import {Avatar, Box, Grid, Skeleton} from "@mui/material";
 
 const ProductsLoading = () => {
+
     return (
-        <Box>
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
-        </Box>
+        <Grid container spacing={2} sx={{ marginBottom: '15px' }}>
+            { [...Array(12)].map((a, index) =>
+                (
+                        <Grid item xs={6} lg={4} id={index}>
+                            <Skeleton sx={{ height: 190, mb: '10px' }} animation="wave" variant="rectangular" />
+                            <Box>
+                                <Skeleton animation="wave" height={10} style={{ marginBottom: 6 }} />
+                                <Skeleton animation="wave" height={10} width="80%" />
+                            </Box>
+                        </Grid>
+                    )
+            )}
+        </Grid>
     );
 };
 
